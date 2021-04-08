19214dd976266462e9c945b5ddd1f5784ff738c4
artemismagilov <72244814+artemismagilov@users.noreply.github.com>
2021-04-08 17:27:39 +0300

    fixed and expanded
    argument file_name optional
    can load all sheets with command [all]
    you can specify one or more range values
    if not pass file_name argument or file_name not found then wrtied stdout

--- GoogleCloud/README.txt
+++ GoogleCloud/README.txt
@@ -1,7 +1,4 @@
-instruction: add the arguments in strict sequence -
-→ sheet name
-→ range data
-→ secret key
-→ id sheet in link
-→ filename
-example: [path]/python3 macros.py Лист1 A1:C7 secret_key.json 000000100110 report.txt
+Instruction: add the arguments in strict sequence - [sheet name,sheet name2,...] or all→ [range data,range data2,...]→
+secret key→ id sheet in link→ filename(optional).
+Example: [path]/python3 spreadsheet.py [Sheet1,Sheet2,...] [A1:C7,...] secret_key.json 273839dsdad033901
+[path/]report.[txt|csv](optional)
--- GoogleCloud/spreadsheet.py
+++ GoogleCloud/spreadsheet.py
@@ -4,53 +4,71 @@ import httplib2
 import argparse
 import csv
 import os
+import re
 import sys
 
 parser = argparse.ArgumentParser(epilog="""
-                         instruction: add the arguments in strict sequence - sheet name→ range data→ secret key→ 
-                         id sheet in link→ filename(optional).\n
-                         example: [path]/python3 spreadsheet.py Sheet1 A1:C7 secret_key.json 273839dss3dad033901 
-                         [path/]report.[txt|csv](optional)
-                         """)
+Instruction: add the arguments in strict sequence - [sheet name,sheet name2,...] or all→ [range data,range data2,...]→ 
+secret key→ id sheet in link→ filename(optional). 
+Example: [path]/python3 spreadsheet.py [Sheet1,Sheet2,...] [A1:C7,...] secret_key.json 273839dsdad033901 
+[path/]report.[txt|csv](optional)
+""")
+
 parser.add_argument("name_sheet", help="name sheet in Google Sheets")
 parser.add_argument("range_data", help="specify the data range in the sheet in Google Sheets")
 parser.add_argument("secret_key", help="the name of the downloaded file with the private key")
 parser.add_argument("id_sheet", help="the id name from your table link")
 parser.add_argument("--file_name", "-f", help="file name with its extension (optional)", default=0)
 args = parser.parse_args()
+
 if os.path.exists(args.secret_key):
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(args.secret_key,
-                                                                   ['https://www.googleapis.com/auth/spreadsheets',
-                                                                    'https://www.googleapis.com/auth/drive'])
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        args.secret_key, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+    )
 else:
     print(f'A non-existent secret_key was passed {args.secret_key}. The file must be in json format')
     quit()
 httpAuth = credentials.authorize(httplib2.Http())
 service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
-range_name = args.name_sheet + '!' + args.range_data
-try:
-    table = service.spreadsheets().values().get(spreadsheetId=args.id_sheet, range=range_name).execute()
-except:
-    print(f'Invalid values passed {args.id_sheet} {range_name}. range_name must be in the format - A1:C7 or '
-          f'wrong id_sheet. The link may be restricted in access '
-          )
-    quit()
-lines = table.get('values', None)
-if not lines:
-    print(f'There is no such sheet {args.name_sheet}')
-    quit()
-strings = ''
-for line in lines:
-    strings += '\t'.join(line) + '\n'
-if os.path.isfile(args.file_name):
-    with open(file=args.file_name, mode='w', encoding='utf-8') as f:
-        filename, file_extension = os.path.splitext(args.file_name)
-        if file_extension == '.csv':
-            csv_writer = csv.writer(f)
-            for line in lines:
-                csv_writer.writerow(line)
-        elif file_extension == '.txt':
-            f.write(strings)
+
+if args.name_sheet == 'all':
+    try:
+        spreadsheet = service.spreadsheets().get(spreadsheetId=args.id_sheet).execute()
+    except:
+        print('Wrong id_sheet. The link may be restricted in access ')
+        quit()
+    names = [sheet['properties']['title'] for sheet in spreadsheet['sheets']]
 else:
-    print('This file does not exist')
-    sys.stdout.write(strings)
+    names = re.split(',', args.name_sheet.strip('[]'))
+    assert '' not in names, f'Wrong sheet format - {names}'
+ranges_data = re.split(',', args.range_data.strip('[]'))
+
+for name, ranges in zip(names, ranges_data*len(names)):
+    range_name = name+'!'+ranges
+    print(range_name)
+    try:
+        table = service.spreadsheets().values().get(spreadsheetId=args.id_sheet, range=range_name).execute()
+    except:
+        print(f'Invalid values passed {name} {ranges}. Wrong range_name or wrong id_sheet or wrong name_sheet. '
+              'The link may be restricted in access ')
+        quit()
+    lines = table.get('values', None)
+    if not lines:
+        print(f'Empty sheet with title {name} and with a gap {ranges}')
+        quit()
+    if os.path.isfile(args.file_name) and args.file_name != 0:
+        filename, file_extension = os.path.splitext(args.file_name)
+        file = open(file=args.file_name, mode='a', newline='')
+        if file_extension == '.csv':
+            csv_writer = csv.writer(file)
+            csv_writer.writerows(lines)
+        elif file_extension == '.txt':
+            for line in lines:
+                file.write(' '.join(line) + '\n')
+        file.close()
+    elif not os.path.isfile(args.file_name) and args.file_name != 0:
+        print(f'No such file: {args.file_name}. Optional-stdout')
+        [sys.stdout.write(' '.join(line) + '\n') for line in lines]
+    elif args.file_name == 0:
+        print('You did not pass a parameter:file_name. Optional-stdout')
+        [sys.stdout.write(' '.join(line) + '\n') for line in lines]
